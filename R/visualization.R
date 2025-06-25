93a2b1d79d4833b76836ee736e655d81b5b94549
alex-d13 <dietrich.alexander1310@gmail.com>
2025-06-25 13:02:33 +0200

    begin with test extensions

--- R/visualization.R
+++ R/visualization.R
@@ -6,7 +6,15 @@
 #'
 #' 
 results_boxplot <- function(result){
-  
+  if (!is.data.frame(result)) {
+    stop("Input to results_boxplot must be a data frame.")
+  }
+  if (ncol(result) < 1) {
+    stop("Input data frame must have at least one column.")
+  }
+  if (is.null(rownames(result))) {
+    stop("Input data frame must have row names.")
+  }
   result |> 
     tibble::rownames_to_column(var = 'sample') |>
     tidyr::pivot_longer(cols = -sample, names_to = 'celltype') |>
@@ -25,7 +33,15 @@ results_boxplot <- function(result){
 #'
 #' 
 results_barplot <- function(result){
-  
+  if (!is.data.frame(result)) {
+    stop("Input to results_barplot must be a data frame.")
+  }
+  if (ncol(result) < 1) {
+    stop("Input data frame must have at least one column.")
+  }
+  if (is.null(rownames(result))) {
+    stop("Input data frame must have row names.")
+  }
   result |> 
     tibble::rownames_to_column(var = 'sample') |>
     tidyr::pivot_longer(cols = -sample, names_to = 'celltype') |>
@@ -43,9 +59,15 @@ results_barplot <- function(result){
 #'
 #' 
 results_aggregated_boxplot <- function(result){
-  
-  result |> 
-    ggplot2::ggplot(mapping = ggplot2::aes(x=value, y= celltype, fill=method))+
+  if (!is.data.frame(result)) {
+    stop("Input to results_aggregated_boxplot must be a data frame.")
+  }
+  required_cols <- c("celltype", "value", "method")
+  missing_cols <- setdiff(required_cols, colnames(result))
+  if (length(missing_cols) > 0) {
+    stop(paste("Input data frame is missing required columns:", paste(missing_cols, collapse=", ")))
+  }
+  ggplot2::ggplot(result, mapping = ggplot2::aes(x=value, y= celltype, fill=method))+
     ggplot2::geom_boxplot()+
     ggplot2::theme_bw()+
     ggplot2::theme(legend.position = 'top')
